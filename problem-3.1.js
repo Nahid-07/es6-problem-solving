652b4a470dd2a9d00b92fa670c4153188e650570
Nahid-07 <nahidasif1998@gmail.com>
2022-08-25 14:54:34 +0600

    problem solved

---
+++ problem-3.1.js
@@ -0,0 +1,11 @@
+/* 
+1) You have an array [ 1, 9, 17, 22 ]. Add the all elements
+of this array and give output. Do this using for loop &
+array.reduce() method.
+*/
+
+const myArray = [ 1, 9, 17, 22 ];
+const sumOfArray = myArray.reduce((previousNumber , currentNumber)=>{
+    return previousNumber+currentNumber
+});
+console.log(sumOfArray)
